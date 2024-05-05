1bda75cc4e17f3a64e22e36f15bfea9f054016fd
Feng, Shun <723278075@qq.com>
2024-05-05 22:44:19 +0800

    feat: V08 实现 props 更新
    - React.js 对比新旧 dom 来更新 props
    - App.jsx 新增代码测试 React 的更新机制

--- V08/App.jsx
+++ V08/App.jsx
@@ -1,13 +1,23 @@
 import React from "./core/React.js"
 
+// 在这个例子中，`count` 被定义在组件外部，作为一个全局变量。这是因为在这个简化的 React 模型中，没有使用 React 的 `useState` Hook 来管理状态，而是直接操作了全局变量 `count` 来模拟状态的更新。
+
+// 在真实的 React 应用中，我们通常不会这样做，因为这样的代码很难维护和理解，也容易引发 bug。我们通常会使用 `useState` 或 `useReducer` Hook 来管理组件的状态。
+
+// 这个例子可能是为了演示 React 的更新机制，而简化了状态管理的部分。在点击按钮后，`count` 的值会增加，然后调用 `React.update()` 来触发组件的重新渲染。
+let count = 10
+let props = { id: "111" }
 function Counter({ num }) {
   function handleClick() {
     console.log("click")
+    count++ // 模拟 state 更新
+    props = {} // 模拟 props 删除
+    React.update()
   }
 
   return (
-    <div>
-      count: {num}
+    <div {...props}>
+      count: {count}
       <button onClick={handleClick}>click</button>
     </div>
   )
--- V08/core/React.js
+++ V08/core/React.js
@@ -46,18 +46,21 @@ function createElement(type, props, ...children) {
  * @param {*} container 真实的 DOM 节点
  */
 function render(el, container) {
-  nextWorkOfUnit = {
+  wipRoot = {
     dom: container,
     props: {
       children: [el]
     }
   }
 
-  root = nextWorkOfUnit
+  nextWorkOfUnit = wipRoot
 }
 
-// 整个应用的根节点
-let root = null
+// work in progress
+// 正在工作中的根节点（在 render 函数中进行了赋值，然后一直在 workLoop 函数中进行构建）
+let wipRoot = null
+// 当前应用的根节点
+let currentRoot = null
 // 任务单元
 let nextWorkOfUnit = null
 function workLoop(deadline) {
@@ -71,8 +74,8 @@ function workLoop(deadline) {
   }
 
   // 计算结束（没有任务单元，并且整个应用的根节点存在）将所有的 dom 添加到整个应用的根节点上
-  // * 添加到视图上的行为，应该只执行一次，所以这里需要判断 root 是否存在
-  if (!nextWorkOfUnit && root) {
+  // * 添加到视图上的行为，应该只执行一次，所以这里需要判断 wipRoot 是否存在
+  if (!nextWorkOfUnit && wipRoot) {
     commitRoot()
   }
 
@@ -84,9 +87,11 @@ function workLoop(deadline) {
  * 将所有的 dom 添加到整个应用的根节点上
  */
 function commitRoot() {
-  commitWork(root.child)
+  commitWork(wipRoot.child)
+  // 在 dom 全部添加完毕之后记录下来。以便在下一次更新时，可以比较新旧 root 的差异
+  currentRoot = wipRoot
   // 当 dom 添加到整个应用的根节点上后，将 root 重置
-  root = null
+  wipRoot = null
 }
 
 /**
@@ -105,16 +110,20 @@ function commitWork(fiber) {
     fiberParent = fiberParent.parent
   }
 
-  // 将当前 fiber 节点的 DOM 元素添加到其父节点的 DOM 元素上
-  if (fiber.dom) {
-    fiberParent.dom.append(fiber.dom)
+  if (fiber.effectTag === "UPDATE") {
+    updateProps(fiber.dom, fiber.props, fiber.alternate?.props)
+  } else if (fiber.effectTag === "PLACEMENT") {
+    // 将当前 fiber 节点的 DOM 元素添加到其父节点的 DOM 元素上
+    if (fiber.dom) {
+      fiberParent.dom.append(fiber.dom)
+    }
   }
 
   commitWork(fiber.child)
   commitWork(fiber.sibling)
 }
 
-/** 封装函数createDom，updateProps，initChildren */
+/** 封装函数createDom，updateProps，reconcileChildren */
 
 /**
  * 创建一个真实的 DOM 节点
@@ -130,18 +139,48 @@ function createDom(type) {
 /**
  * 将虚拟 DOM 对象的属性更新到真实的 DOM 节点上
  * @param {*} dom 真实的 DOM 节点
- * @param {*} props 节点属性
+ * @param {*} nextProps 节点属性
+ * @param {*} prevProps 之前的节点属性
  */
-function updateProps(dom, props) {
-  Object.keys(props).forEach((key) => {
+function updateProps(dom, nextProps, prevProps) {
+  // Object.keys(props).forEach((key) => {
+  //   if (key !== "children") {
+  //     if (key.startsWith("on")) {
+  //       // 处理：删除前两个字符，将剩余的字符串转换为小写 onClick -> click
+  //       const eventType = key.slice(2).toLowerCase()
+  //       // 添加事件监听
+  //       dom.addEventListener(eventType, props[key])
+  //     } else {
+  //       dom[key] = props[key]
+  //     }
+  //   }
+  // })
+  // 比如老的有id，新的没有id，需要删除 {id: 1} {id: undefined}
+  // 1. old 有，new 没有，需要删除
+  // 遍历老的属性，如果它在新的属性中没有，就需要删除
+  Object.keys(prevProps).forEach((key) => {
     if (key !== "children") {
-      if (key.startsWith("on")) {
-        // 处理：删除前两个字符，将剩余的字符串转换为小写 onClick -> click
-        const eventType = key.slice(2).toLowerCase()
-        // 添加事件监听
-        dom.addEventListener(eventType, props[key])
-      } else {
-        dom[key] = props[key]
+      if (!(key in nextProps)) {
+        dom.removeAttribute(key)
+      }
+    }
+  })
+
+  // 2. new 有，old 没有，需要添加
+  // 3. new 有，old 有，但是值不同，需要更新
+  // 2和3可以合并，因为没有就是 undefined，它和 new[key] 也是不相等
+  Object.keys(nextProps).forEach((key) => {
+    if (key !== "children") {
+      // 如果新旧属性值不同，就更新
+      if (nextProps[key] !== prevProps[key]) {
+        if (key.startsWith("on")) {
+          const eventType = key.slice(2).toLowerCase()
+          // 移除之前的事件监听，添加新的事件监听
+          dom.removeEventListener(eventType, prevProps[key])
+          dom.addEventListener(eventType, nextProps[key])
+        } else {
+          dom[key] = nextProps[key]
+        }
       }
     }
   })
@@ -151,21 +190,49 @@ function updateProps(dom, props) {
  * 构建一个虚拟 DOM 树的链表结构。
  * * 这个结构可以帮助我们在后续的渲染和更新过程中，更方便地遍历和操作虚拟 DOM 树。
  * * fiber 对象是一个链表结构，包含了当前节点的所有信息。特别是 parent，child，sibling 这三个指针，用来构建节点之间的关系
+ * * reconcile 使一致，协调，调和
  * @param {*} fiber 当前节点
  * @param {Array} children 当前节点的全部子节点
  */
-function initChildren(fiber, children) {
-  console.log("initChildren", fiber)
+function reconcileChildren(fiber, children) {
+  // 注意：初始化的时候，fiber.alternate 是没有值的，所以使用可选链操作符
+  let oldFiber = fiber.alternate?.child
 
   let prevChild = null // 记录上一个子节点
   children.forEach((child, index) => {
-    const newFiber = {
-      type: child.type,
-      props: child.props,
-      child: null, // 存储第一个子节点
-      parent: fiber,
-      sibling: null,
-      dom: null // 存储与 fiber 对应的真实 DOM 节点
+    const isSameType = oldFiber && oldFiber.type === oldFiber.type
+
+    let newFiber
+
+    // 如果是同一类型，那么 React 就会复用旧的节点（fiber），并更新属性。如果不是同一类型，那么 React 就会销毁旧的节点，并创建一个新的节点。
+    if (isSameType) {
+      // update 更新
+      newFiber = {
+        type: child.type,
+        props: child.props,
+        child: null, // 存储第一个子节点
+        parent: fiber,
+        sibling: null,
+        dom: oldFiber.dom, // 复用旧的 DOM 节点（更新是不会新创建 DOM 节点的）
+        effectTag: "UPDATE", // 标记节点的操作类型
+        alternate: oldFiber // 将旧的 fiber 节点存储起来，以便在下一次更新时，可以比较新旧 fiber 节点的差异
+      }
+    } else {
+      // add 创建
+      newFiber = {
+        type: child.type,
+        props: child.props,
+        child: null, // 存储第一个子节点
+        parent: fiber,
+        sibling: null,
+        dom: null, // 存储与 fiber 对应的真实 DOM 节点
+        effectTag: "PLACEMENT" // 标记节点的操作类型
+      }
+    }
+
+    // 在 Fiber 树的同一层级中移动到下一个节点
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling
     }
 
     // 将第一个节点设置为父节点的 child 属性，其他节点设置为上一个节点的 sibling 属性。这样就形成了一个链表结构。
@@ -190,7 +257,7 @@ function updateFunctionComponent(fiber) {
   const children = [fiber.type(fiber.props)]
 
   // 转换成链表
-  initChildren(fiber, children)
+  reconcileChildren(fiber, children)
 }
 
 /**
@@ -198,16 +265,16 @@ function updateFunctionComponent(fiber) {
  * @param {*} fiber
  */
 function updateHostComponent(fiber) {
-  // 根据设计，初次进来 fiber.dom 真实节点已存在（如root），所以这里会跳过，直接进入 initChildren
+  // 根据设计，初次进来 fiber.dom 真实节点已存在（如root），所以这里会跳过，直接进入 reconcileChildren
   if (!fiber.dom) {
     const dom = (fiber.dom = createDom(fiber.type))
 
-    updateProps(dom, fiber.props)
+    updateProps(dom, fiber.props, {})
   }
 
   // 转换成链表
   const children = fiber.props.children
-  initChildren(fiber, children)
+  reconcileChildren(fiber, children)
 }
 
 /**
@@ -242,7 +309,22 @@ function performanceWorkUnit(fiber) {
 
 requestIdleCallback(workLoop)
 
+/**
+ * 用于更新组件的状态
+ * * 在 React 中，更新组件的状态是通过调用 setState 方法来实现的。在这里，我们暂时简化了这个过程，直接调用 update 方法来更新组件的状态。
+ */
+function update() {
+  wipRoot = {
+    dom: currentRoot.dom,
+    props: currentRoot.props,
+    alternate: currentRoot
+  }
+
+  nextWorkOfUnit = wipRoot
+}
+
 const React = {
+  update,
   render,
   createElement
 }
