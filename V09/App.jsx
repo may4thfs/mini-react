4116deaf57b715c9a61388ab382159f2b7955383
Feng, Shun <723278075@qq.com>
2024-05-26 15:18:27 +0800

    chore: 拷贝出 V09 初始模板

---
+++ V09/App.jsx
@@ -0,0 +1,31 @@
+import React from "./core/React.js"
+
+let showBar = false
+function Counter() {
+  const foo = <div>foo</div>
+  const bar = <p>bar</p>
+
+  function handleShowBar() {
+    showBar = !showBar
+    React.update()
+  }
+
+  return (
+    <div>
+      Counter
+      <div>{showBar ? bar : foo}</div>
+      <button onClick={handleShowBar}>showBar</button>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      hi-mini-react
+      <Counter></Counter>
+    </div>
+  )
+}
+
+export default App
